5fc3b2cc332ef81a1c25f0bb21d84bd362a85ed3
jjhelguero <joshua.helguero@mercari.com>
2023-01-07 20:31:37 -0600

    add task and script

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      console.log(process.env)
       on("task", {
         'getImportantToken'() {
           return process.env.IMPORTANT_TOKEN
